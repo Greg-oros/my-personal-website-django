339daa7806584eaa9b2d9ebac24018b522154dd3
Greg Oros <g.olczyk@yahoo.com>
2025-12-19 19:33:41 +0100

    Update: Gotowa wersja responsywna i konfiguracja static

--- moja_strona/settings.py
+++ moja_strona/settings.py
@@ -27,9 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = [
+    "olczyk-innowacje.pl",
+    "www.olczyk-innowacje.pl",
+    "46.224.41.184",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
